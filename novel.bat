321c23d669a4a21824e8c36a4f6939cd7e0fe142
Bartender <bartender.winery@gmail.com>
2021-12-25 16:58:20 -0500

    Finished beta

--- novel.bat
+++ novel.bat
@@ -1,12 +1,12 @@
-@echo off && mode con: cols=120 lines=25 && call cls && title Visual Novel - Batch
+@echo off && mode con: cols=150 lines=35 && call cls && title Visual Novel - Batch
 if "%~1"=="" (
-    echo ==---=Max novel size=-------------------------------------------------------------------------------------------------==
+    echo ==---=Max novel size=-----------------------------------------------------------------------------------------------------------------------------==
     echo Please select a novel story. && echo: && call :list && goto :input
 ) else if "%~1"=="console" (
     goto :con
     exit /b 0
 ) else if "%~1"=="load" (
-    goto :load
+    call %input%.cmd
     exit /b 0
 )
 :input
@@ -14,13 +14,16 @@ if "%~1"=="" (
     set /p input=">>: "
     rem check for commands
         if %input%==console:: ( set command=1 && echo: && echo Opening console... && call :console )
-        if %input%==exit:: ( set command=1 && echo: && echo Rate your experience on this project at: && echo https://github.com/BartenderWinery/Batch-novel && echo: && echo ==---=End script=-----------------------------------------------------------------------------------------------------== && timeout /t 3 /nobreak>nul && exit )
+        if %input%==exit:: ( set command=1 && echo: && echo Rate your experience on this project at: && echo https://github.com/BartenderWinery/Batch-novel && echo: && echo ==---=End script=--------------------------------------------------------------------------------------------------------== && timeout /t 3 /nobreak>nul && exit )
         if %input%==cls:: ( set command=1 && call cls )
-        if %input%==ref:: ( set command=1 && echo Maximum arcii art: && echo: && echo ==---=Reference=------------------------------------------------------------------------------------------------------==)
+        if %input%==ref:: ( set command=1 && echo Maximum arcii art: && echo: && echo ==---=Reference=-----------------------------------------------------------------------------------------------------------------------------==)
+        if %input%==list:: ( set command=1 && call :list )
     rem check for novel by name
         if %command%==0 (
-            if exist %input%.cmd ( goto :load )
-            if exist %input%.novel ( echo: && echo Found %input%; Loading... && call :load
+            if exist %input%.cmd ( echo Found %input%; Loading... && echo ==------------------------------------------------------------------------------------------------------------------------------------------------== && call %input%.cmd 
+            ) else ( 
+                if not exist %input%.cmd ( if exist %input%.novel ( type %input%.novel >> %input%.cmd && del %input%.novel ) ) else ( echo Ignored %input%.novel; running cmd file instead. ) ) 
+            if exist %input%.novel ( if not exist %input%.cmd ( echo: && echo Found %input%; Loading... && call %input%.cmd )
                 ) else ( echo: && echo No novel with the name: "%input%" was found! && echo Please make sure you have the novel file in the same directory as the batch file.)
             )
     goto input
@@ -36,18 +39,12 @@ if "%~1"=="" (
     start cmd /k novel.bat console
     exit /b 0
 :con
-    echo ==--------------------------------------------------------------------------------------------------------------------==
+    echo ==---------------------------------------------------------------------------------------------------------------------------------------------==
     set /p rcon=">>>: "
     rem check for commands
         if %rcon%==init ( echo Spawning new window... && start cmd /k novel.bat )
         if %rcon%==cls ( call cls )
         if %rcon%==exit ( call exit )
         if %rcon%==restore ( set /p input="Restore:" && echo Restoring CMD files back to .novel... && type %input%.cmd >> %input%.novel && del %input%.cmd )
+        if %rcon%==create ( echo Generating novels not yet supported. )
     goto :con
-:load
-    echo ==--------------------------------------------------------------------------------------------------------------------==
-    set line=0
-    if not exist %input%.cmd ( if exist %input%.novel ( type %input%.novel >> %input%.cmd && del %input%.novel ) ) else ( echo Ignored %input%.novel; running cmd file instead. && echo: )
-    call %input%.cmd
-    set /p text="Say: "
-    goto :load
